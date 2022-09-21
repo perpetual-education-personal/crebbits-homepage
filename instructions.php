7f394eaa185d5cf9fbf3ac7113a41deff6a8bc16
Tara <tara.haskamp@gmail.com>
2022-09-21 18:16:27 -0500

    Add instructions php functions to load json data

--- instructions.php
+++ instructions.php
@@ -1,3 +1,11 @@
+<?php
+	$json = file_get_contents("instructions.json");
+	$pageData = json_decode($json, true);
+
+	// var_dump($pageData);
+
+?>
+
 <?php include('header.php'); ?>
 
 
@@ -8,11 +16,43 @@
 	<h1 class='loud-voice'>Instructions</h1>
 
 	<ol>
-		<li>
-			<section>
-				EACH SECTION WOULD GO HERE
-			</section>
-		</li>
+			<?php	
+				foreach($pageData as $section) { 
+					if (isset($section['heading'])) {
+						$heading = $section['heading'];
+					}
+
+					if (isset($section['content'])) {
+						$content = $section['content'];
+					}
+					
+					if (isset($section['supportingImage'])) {
+						$image = $section['supportingImage'];
+					}
+					
+					if (isset($section['bonus']))	{
+						$bonus = $section['bonus'];
+					}
+				}
+			?>
+				<li>
+					<section>
+						<h2 class='attention-voice'><?=$heading?></h2>
+
+						<?php foreach ($content as $paragraph) { ?>
+							<p><?=$paragraph?></p>
+						<?php	} ?>
+
+						<picture>
+							<img src="https://www.peprojects.dev/images/square.jpg" alt="">
+						</picture>
+
+						<div class="bonus">
+							<p><?=$bonus?></p>
+						</div>
+					</section>
+				</li>
+			<?php  ?>		
 	</ol>
 
 
