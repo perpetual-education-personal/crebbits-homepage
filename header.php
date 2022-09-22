9125b2ebc42ea096f29653be06d939e7ef39f209
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-09-21 21:50:17 -0500

    edit header styles

--- header.php
+++ header.php
@@ -16,23 +16,23 @@
 
 	<body>
 		<header>
-			<!-- <h1 class='loud-voice'>Crebbits</h1> -->
+		<inner-column>
+				<nav>
+					<ul>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
 
-			<nav>
-				<ul>
-					<li>
-						<a href="index.php">Home</a>
-					</li>
+						<li>
+							<a href="instructions.php">Instructions</a>
+						</li>
 
-					<li>
-						<a href="instructions.php">Instructions</a>
-					</li>
-
-					<li>
-						<a href="store.php">Store</a>
-					</li>
-				</ul>
-			</nav>
+						<li>
+							<a href="store.php">Store</a>
+						</li>
+					</ul>
+				</nav>
+		</inner-column>
 		</header>
 
 		<main>
