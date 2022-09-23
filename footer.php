e8073fa66050edafd382ae542872106a954b4ed0
Stevedeanda <stevedeanda5@gmail.com>
2022-09-23 14:18:41 -0500

    Changed the footer to match the header styling

--- footer.php
+++ footer.php
@@ -4,23 +4,23 @@
 		<footer>
 		<inner-column>
 			<nav>
-					<ul>
-						<picture>
-							<a href="index.php"><img src="images/boardgame/blue-logo.svg" alt=""></a>
-						</picture>
-						<li>
-							<a href="index.php">Home</a>
-						</li>
+				<picture>
+					<a href="index.php"><img src="images/boardgame/blue-logo.svg" alt=""></a>
+				</picture>
+				<ul>
+					<li>
+						<a href="index.php">Home</a>
+					</li>
 
-						<li>
-							<a href="instructions.php">Instructions</a>
-						</li>
+					<li>
+						<a href="instructions.php">Instructions</a>
+					</li>
 
-						<li>
-							<a href="store.php">Store</a>
-						</li>
-					</ul>
-				</nav>
+					<li>
+						<a href="store.php">Store</a>
+					</li>
+				</ul>
+			</nav>
 		</inner-column>
 		</footer>
 
