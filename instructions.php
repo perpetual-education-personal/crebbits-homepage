68092f7d4b4dc83c04743a6fc44e275c8d718d68
Tara <tara.haskamp@gmail.com>
2022-09-22 22:08:43 -0500

    Update images json and php to allow for multiple

--- instructions.php
+++ instructions.php
@@ -16,47 +16,49 @@
 	<h1 class='loud-voice'>Instructions</h1>
 
 	<ol>
-			<?php	
-				foreach($pageData as $section) { 
-					if (isset($section['heading'])) {
-						$heading = $section['heading'];
-					}
+		<?php	
+			foreach($pageData as $section) { 
+				if (isset($section['heading'])) {
+					$heading = $section['heading'];
+				}
 
-					if (isset($section['content'])) {
-						$content = $section['content'];
-					}
-					
-					if (isset($section['supportingImage'])) {
-						$image = $section['supportingImage'];
-					}
-					
-					if (isset($section['tips']))	{
-						$tips = $section['tips'];
-					} ?>
-				<li>
-					<section>
-						<h2 class='attention-voice'><?=$heading?></h2>
+				if (isset($section['content'])) {
+					$content = $section['content'];
+				}
+				
+				if (isset($section['supportingImage'])) {
+					$images = $section['supportingImage'];
+				}
+				
+				if (isset($section['tips']))	{
+					$tips = $section['tips'];
+				} ?>
+			<li>
+				<section>
+					<h2 class='attention-voice'><?=$heading?></h2>
 
-						<?php foreach ($content as $paragraph) { ?>
-							<p><?=$paragraph?></p>
-						<?php	} ?>
+					<?php foreach ($content as $paragraph) { ?>
+						<p><?=$paragraph?></p>
+					<?php	} ?>
 
-						<?php if (isset($image)) { ?>
+					<?php if (isset($images)) { ?>
+						<?php foreach ($images as $image) { ?>
 							<picture class='supporting-image'>
 								<img src="<?=$image?>" alt="">
 							</picture>
 						<?php } ?>
+					<?php } ?>
 
-						<?php	if (isset($tips)) { ?>
-							<?php foreach ($tips as $tip) { ?>
-								<div class="tip">
-									<p><?=$tip?></p>
-								</div>
-							<?php } ?>
-						<?php	} ?>
-					</section>
-				</li>
-			<?php } ?>		
+					<?php	if (isset($tips)) { ?>
+						<?php foreach ($tips as $tip) { ?>
+							<div class="tip">
+								<p><?=$tip?></p>
+							</div>
+						<?php } ?>
+					<?php	} ?>
+				</section>
+			</li>
+		<?php } ?>		
 	</ol>
 
 
