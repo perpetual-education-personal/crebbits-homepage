e943646f0c8b0191fb8f5df8b506f6250e342bff
Stevedeanda <stevedeanda5@gmail.com>
2022-09-23 12:56:02 -0500

    Styled the footer a little and put the site map

--- footer.php
+++ footer.php
@@ -3,7 +3,22 @@
 
 		<footer>
 		<inner-column>
-			<h2>Zee footah</h2>
+			<nav>
+					<ul>
+						<li>CREBBITS</li>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+
+						<li>
+							<a href="instructions.php">Instructions</a>
+						</li>
+
+						<li>
+							<a href="store.php">Store</a>
+						</li>
+					</ul>
+				</nav>
 		</inner-column>
 		</footer>
 
