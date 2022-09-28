5bfd5032b3ccce08aefa077777eb9613c5eaa463
Tara <tara.haskamp@gmail.com>
2022-09-27 19:43:43 -0500

    Update instructions for better image placement

--- instructions.php
+++ instructions.php
@@ -1,3 +1,4 @@
+
 <?php
 	$json = file_get_contents("instructions.json");
 	$pageData = json_decode($json, true);
@@ -9,10 +10,8 @@
 <?php include('header.php'); ?>
 
 
-<section class='section-name' id='and-its-id'>
+<section class='instructions' id='instructions'>
 <inner-column>
-
-
 	<h1 class='loud-voice'>Instructions</h1>
 
 	<ol class='instructions'>
@@ -75,18 +74,11 @@
 			</li>
 		<?php } ?>		
 	</ol>
-
-
 </inner-column>
 </section>
 
 
-<section class='buy'>
-<inner-column>
 
 <?php include ('modules/buy-it/buy-it.php'); ?>
-
-</inner-column>
-</section>
 
 <?php include('footer.php'); ?>
