4e90cad16239159f6c62b40f8297d67c10f66449
Tara <tara.haskamp@gmail.com>
2022-09-27 22:21:52 -0500

    Move instructions away from json file type

---
+++ how-to-play.php
@@ -0,0 +1,125 @@
+<section class='instructions' id='instructions'>
+<inner-column>
+	<h1 class='loud-voice'>Instructions</h1>
+	
+	<section>
+		<h2 class='instructions attention-voice'>Goal of the Game</h2>
+
+		<p>End up with the most points after three rounds.</p>
+
+		<p>This game has two versions – basic and advanced. Start with basic until you’re comfortable then move on to the advanced version!</p>
+	</section>
+
+	<section>
+		<h2 class='instructions attention-voice'>Basic Version Setup</h2>
+
+		<ol>
+			<li>1. Remove the cards that say “Steal”, “Trade” and “Wild”. These are only used in the advanced version.</li>
+
+			<li>2. Keep the blue and red decks separate.</li>
+
+			<li>3. Shuffle each deck and set them in the middle of the table. Each deck has its own discard pile next to it.</li>
+		</ol>
+		
+		<picture><img src="images/cards/piles.png" alt=""></picture>		
+	</section>
+
+	<section>
+		<h2 class='instructions attention-voice'>Start of Game</h2>
+
+		<p>Each player draws 5 blue cards (credits) and 5 red cards (debits). Don’t show the other players your cards!</p>
+	</section>
+
+	<section>
+		<h2 class='instructions attention-voice'>How to Play a Turn</h2>
+		<ol>
+			<li>1. Each turn you will draw one card, red or blue. You can choose the top card from either the draw pile or discard pile.</li>
+
+			<li>
+				<p>2. You will play matches by laying them down side by side in front of you.</p>
+
+				<picture><img src='images/cards/cards-1000.png'></picture>
+			</li>
+
+				
+
+			<li>3. You can choose to play any number of matches per turn or no matches at all. Each subsequent match is stacked on top of the old match but you must alternate the colors.</li>
+
+			<li>
+				<picture><img src='images/cards/stacks.png'></picture>
+			</li>
+
+			<li>4. To end your turn, you will discard a card of the same color that you drew at the beginning of that turn, by placing it face up in the discard pile. You must end your turn with an equal number of blue and red cards in your hand.</li>
+
+			<li>5. After discarding, it is the next player's turn.</li>
+
+			<li>6. If the draw pile runs out, leave the top card, pick up the rest of the discard pile, shuffle and place the cards face down as the new draw pile.</li>
+		</ol>
+	</section>
+
+	<section>
+		<h2 class='instructions attention-voice'>Basic gameplay</h2>
+
+		<p>Match your blue cards with your red cards to create matches. Matches can only be created with corresponding values (Ex. Red <1,000> must be matched to Blue 1,000.) But be careful! The order you stack your matches matters and red cards will count against your score at the end of the round, so play smart! Once you play a match you cannot rearrange the order of your matches.</p>
+	</section>
+
+	<section>
+		<h2 class='instructions attention-voice'>End of Round</h2>
+
+		<p>The round ends when a player runs out of cards in their hand. Once the round ends you calculate the score for that round.</p>
+	</section>
+
+	<section>
+		<h2 class='instructions attention-voice'>Calculating Score </h2>
+
+		<p>Both stacks in front of you will have a score. The stack with the highest number will be your stack score for the round. The smaller amount is ignored and will not be used for anything.</p>
+		
+		<p>The blue cards are positive and will add to your score - the red cards are negative and will subtract from your score. The stack with the highest score is known as the “Stack Score”, which you will use to find your total score for the round. In the example Stack 1 has the highest number and will be used as the “Stack Score” for the round.</p>
+		
+		<p>Any player who still has cards in their hand must add up the values on the red cards to get their “hand score” which will be subtracted from their stack score. This could result in a negative score for the round!</p>
+		
+		<h3 class="special-title">play again</h3>
+
+		<p class='special-info'>Play 3 rounds (or a different number if you want) and add up each player’s score from each round. The player with the highest score at the end of 3 rounds wins!</p>
+								
+		<picture class='supporting-image'>
+			<img src="images/cards/stacks.png" alt="">
+		</picture>
+								
+	</section>
+
+	<section>
+		<h2 class='instructions attention-voice'>Advanced Version</h2>
+
+			<p>For the advanced version, shuffle the “Wild”, “Steal”, and “Trade” cards into the decks that match their color.</p>
+		
+			<h3 class="special-title">wild</h3>
+
+			<p class='special-info'>“Wild” cards can be used in place of any number to make a match. You can use a red “Wild” and a blue 10,000 to make a match for example! Be careful though! A red “Wild” in your hand at the end of a round is <10,000> deducted from your score!</p>
+			
+			<h3 class="special-title">trade</h3>
+
+			<p class='special-info'>When a “Trade” card is played you pick any two players (you can pick yourself as one but you don’t have to) who will look at each other’s hands and take a card from the other player that matches the color of the “Trade” card to keep for themselves. After they trade, the player who played the “Trade” card draws from the deck that matches the color of the “Trade” card and then places the “Trade” card on the bottom of the draw pile to end the turn.</p>
+			
+			<h3 class="special-title">steal</h3>
+
+			<p class='special-info'>When a “Steal” card is played you pick one other player. Look at their hand and take one card that matches the color of the “Steal” card you played. The player stolen from then draws from the top of the draw pile that matches the color of the “Steal” card. The discard pile is off limits. Then, place the “Steal” card on the bottom of the draw pile to end the turn.</p>
+								
+			<picture class='supporting-image'>
+				<img src="images/cards/stacks.png" alt="">
+			</picture>
+								
+			<div class="tip">
+				<h4>Tip!</h4>
+				
+				<p><em>“Trade” and “Steal” cards can be played on your turn without making a match. They can also be discarded without being used if desired. If they are drawn at the beginning of a turn, they can be played in that turn.</em></p>
+			</div>
+			
+			<div class="tip">
+				<h4>Tip!</h4>
+				
+				<p><em>A red “Steal” or “Trade” card in your hand at the end of the round is <5,000> for your score so don’t hold onto them for too long!</em></p>
+				</div>
+	</section>
+</inner-column>
+</section>
--- instructions.php
+++ instructions.php
@@ -10,72 +10,7 @@
 <?php include('header.php'); ?>
 
 
-<section class='instructions' id='instructions'>
-<inner-column>
-	<h1 class='loud-voice'>Instructions</h1>
-
-	<ol class='instructions'>
-		<?php	
-			foreach($pageData as $section) { 
-				if (isset($section['heading'])) {
-					$heading = $section['heading'];
-				}
-
-				if (isset($section['content'])) {
-					$content = $section['content'];
-				}
-				
-				if (isset($section['supportingImage'])) {
-					$images = $section['supportingImage'];
-				}
-				
-				if (isset($section['tips']))	{
-					$tips = $section['tips'];
-				} 
-
-				if (isset($section['specials'])) {
-					$specials = $section['specials'];
-				}
-		?>
-			<li>
-				<section>
-					<h2 class='attention-voice'><?=$heading?></h2>
-
-					<?php foreach ($content as $paragraph) { ?>
-							<p><?=$paragraph?></p>
-					<?php	} ?>
-
-					<?php if (isset($specials)) { ?>
-						<?php foreach ($specials as $special) { ?>
-							<h3 class="special-title"><?=$special['title']?></h3>
-
-							<p class='special-info'><?=$special['info'] ?></p>
-						<?php } ?>
-					<?php } ?>
-
-					<?php if (isset($images)) { ?>
-						<?php foreach ($images as $image) { ?>
-							<picture class='supporting-image'>
-								<img src="<?=$image?>" alt="">
-							</picture>
-						<?php } ?>
-					<?php } ?>
-
-					<?php	if (isset($tips)) { ?>
-						<?php foreach ($tips as $tip) { ?>
-							<div class="tip">
-								<h4>Tip!</h4>
-								
-								<p><em><?=$tip?></em></p>
-							</div>
-						<?php } ?>
-					<?php	} ?>
-				</section>
-			</li>
-		<?php } ?>		
-	</ol>
-</inner-column>
-</section>
+<?php include('how-to-play.php'); ?>
 
 
 
