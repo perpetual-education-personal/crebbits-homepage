b25bd69a693533dcff7c5d396798c4fcbe8c2055
Tara <tara.haskamp@gmail.com>
2022-10-03 17:59:51 -0500

    Remove json structure

--- instructions.php
+++ instructions.php
@@ -1,19 +1,5 @@
 
-<?php
-	$json = file_get_contents("instructions.json");
-	$pageData = json_decode($json, true);
-
-	// var_dump($pageData);
-
-?>
-
 <?php include('header.php'); ?>
-
-
 <?php include('how-to-play.php'); ?>
-
-
-
 <?php include ('modules/buy-it/buy-it.php'); ?>
-
 <?php include('footer.php'); ?>
